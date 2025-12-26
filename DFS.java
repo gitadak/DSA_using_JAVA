c31e36c3303dbc9d3cfcc978958ae9750df9ed11
Soumyadeep Adak <138775532+gitadak@users.noreply.github.com>
2025-12-26 19:15:44 +0530

    Add non-recursive DFS implementation with Adjacency List input

--- DFS.java
+++ DFS.java
@@ -108,3 +108,107 @@ Adjacency Matrix:
 
 DFS Traversal starting from node 1: 1 4 3 5 2
 */
+
+/****************************************************************************************************************/
+
+/*
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+class GfG
+{
+    // Non-recursive DFS using stack (connected graph)
+    static ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj)
+    {
+        int V = adj.size();
+        boolean[] visited = new boolean[V];
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Start DFS from vertex 0
+        stack.push(0);
+
+        while (!stack.isEmpty())
+        {
+            int current = stack.pop();
+
+            if (!visited[current])
+            {
+                visited[current] = true;
+                result.add(current);
+
+                // Push neighbours in reverse order
+                // to match recursive DFS order
+                for (int i = adj.get(current).size() - 1; i >= 0; i--)
+                {
+                    int neighbour = adj.get(current).get(i);
+                    if (!visited[neighbour])
+                    {
+                        stack.push(neighbour);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    // Add undirected edge
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v)
+    {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of vertices: ");
+        int V = sc.nextInt();
+
+        System.out.print("Enter number of edges: ");
+        int E = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        // Create adjacency list
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < E; i++)
+        {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            addEdge(adj, u, v);
+        }
+
+        ArrayList<Integer> res = dfs(adj);
+
+        System.out.println("\nDFS Traversal:");
+        for (int x : res)
+            System.out.print(x + " ");
+
+        sc.close();
+    }
+}
+*/
+
+/*
+Sample Input
+------------
+Enter number of vertices: 5
+Enter number of edges: 5
+Enter edges (u v):
+1 2
+1 0
+2 0
+2 3
+2 4
+
+Sample Output
+-------------
+DFS Traversal:
+0 1 2 3 4
+*/
